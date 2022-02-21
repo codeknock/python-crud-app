f474fea5eb6795a7b52084260b1d6b91e5d35b92
chandrakant nyaupane <chandra@MacBook-Pro.local>
2022-02-21 13:17:58 +0200

    database created in init.py

--- website/__init__.py
+++ website/__init__.py
@@ -19,10 +19,11 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import models
+    create_database(app)
 
     return app
 
-    def create_database(app):
+def create_database(app):
         if not path.exits('website/' + DB_NAME):
             db.create_all(app=app)
             print('Created Database!')
